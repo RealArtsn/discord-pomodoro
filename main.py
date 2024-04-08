d67458d2228974543cc8bcde3496da795f88cb49
RealArtsn <105236007+RealArtsn@users.noreply.github.com>
2024-04-07 21:16:06 -0400

    Stop timer after vc disconnect

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ import logging, sys, os
 import asyncio
 from datetime import datetime
 
+# TODO: Check if timer already running
+# TODO: Combine start/connect
+# TODO: Make sure timer properly stops
+# TODO: Add pause/stop functionality
+
 class Client(discord.Client):
     def __init__(self, *args, **kwargs):
         super(Client, self).__init__(*args, **kwargs)
@@ -17,7 +22,7 @@ class Client(discord.Client):
                 await voice_client.disconnect()
             # connect to calling user's voice channel and respond
             await interaction.user.voice.channel.connect()
-            response = 'Connected successfully.' if self.voice_clients[0].is_connected() else 'Connecting failed.'
+            response = 'Connected successfully.' if self.is_voice_connected() else 'Connecting failed.'
             await interaction.response.send_message(response ,ephemeral=True)
         
         # Disconnect from all connected voice clients    
@@ -27,14 +32,20 @@ class Client(discord.Client):
             for voice_client in self.voice_clients:
                 await voice_client.disconnect()
 
+
         # connect to channel and set timer
         @self.tree.command(name = "start", description = "Start pomodoro timer.")
         async def slash(interaction:discord.Interaction):
             await interaction.response.send_message('Timer started.', ephemeral=True)
             # run timers until bot is out of vc
-            while len(self.voice_clients) > 0:
+            while self.is_voice_connected(): # does this actually work?
                 await self.set_timer(1200, 'WORK')
+                # stop the loop if voice is not connected
+                if not self.is_voice_connected():
+                    break
                 await self.set_timer(300, 'BREAK')
+            await self.update_status('Waiting...')
+
         # create log directory if not exists
         try:
             os.mkdir('logs')
@@ -65,7 +76,7 @@ class Client(discord.Client):
             await self.close()
 
         # set status to waiting
-        await self.change_presence(activity=discord.Game('Waiting...'))
+        await self.update_status('Waiting...')
 
 
     # channel status timer
@@ -74,21 +85,37 @@ class Client(discord.Client):
             return (datetime.now() - datetime(1970, 1, 1)).total_seconds()
         unix_start = unix_now()
         i = time
-        while unix_now() < unix_start + time:
+        while unix_now() < unix_start + time and self.is_voice_connected():
             # set discord activity to remaining time
-            await self.change_presence(activity=discord.Game(f'{message}: ' + str(int(i/60)) + ' min'))
+            await self.update_status(f'{message}: ' + str(int(i/60)) + ' min')
             i = time - (unix_now() - unix_start)
             # wait 60 seconds or i seconds if less than 1 minute
+            # sleep_time = 60 if i > 60 else i
             if i > 60:
                 sleep_time = 60
             else:
                 sleep_time = i
             await asyncio.sleep(sleep_time)
 
+        # skip ding if voice not connected
+        if not self.is_voice_connected():
+            return
+
+        # play ding sound
         voice_client: discord.VoiceClient = self.voice_clients[0]
-        # await self.change_presence(activity=discord.Game('Waiting...'))
         voice_client.play(discord.FFmpegPCMAudio(source='ding.mp3'))
         return
+    
+    # check if bot is connected to voice channel
+    def is_voice_connected(self):
+        if len(self.voice_clients) < 1:
+            return False
+        return self.voice_clients[0].is_connected()
+
+    # update discord status
+    async def update_status(self, text:str):
+        # use discord game activity to change status
+        await self.change_presence(activity=discord.Game(text))
 
 
 # create bot instance
